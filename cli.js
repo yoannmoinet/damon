d756d8345a0c5a9be69b6d2905b797f8b0dc26de
RuoTai Sun <sunruotai95@gmail.com>
2016-01-21 16:59:06 -0500

    change README and add description to --help

--- cli.js
+++ cli.js
@@ -6,14 +6,17 @@ var pkg = require('./package.json');
 var path = require('path');
 var _ = require('underscore');
 
+program._name = pkg.name.slice(0, -1);
+
 program
     .version(pkg.version)
-    .command('test <files...>')
+    .command('run <files...>')
+    .description('Run the list of JSON tasks files. Accept glob.')
     .action(function (files) {
         var filesList = _.map(files, function (file) {
             return path.join(process.cwd(), file)
         });
         agent.start(filesList);
     });
-    
+
 program.parse(process.argv);
