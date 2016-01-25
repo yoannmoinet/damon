0af3935d44eb6ab4c707b8f229147bfc513f08b3
RuoTai Sun <sunruotai95@gmail.com>
2016-01-25 10:56:24 -0500

    now accept absolute path and add glob now working with the 'glob' module

--- cli.js
+++ cli.js
@@ -4,16 +4,30 @@ var program = require('commander');
 var agent = require('./src/index');
 var pkg = require('./package.json');
 var path = require('path');
+var glob = require("glob")
 
 program._name = pkg.name.slice(0, -1);
+
+function globFiles (file) {
+    var fullPath; 
+    if(path.resolve(file) === path.normalize(file)) {
+        fullPath = file;
+    }
+    else {
+        fullPath = path.join(process.cwd(), file);
+    }
+    return glob.sync(fullPath);
+}
+
+var filesList = [];
 
 program
     .version(pkg.version)
     .command('run <files...>')
     .description('Run the list of JSON tasks files. Accept glob.')
     .action(function (files) {
-        var filesList = files.map(function (file) {
-            return path.join(process.cwd(), file);
+        files.forEach(function (file) {
+            filesList = filesList.concat(globFiles(file));
         });
         agent.start(filesList);
     });
