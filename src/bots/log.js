9b96fb53daa27a3bdf4de7d742e3f405603ba1ef
Yoann Moinet <yoann.moinet@gmail.com>
2016-02-19 14:42:20 -0500

    feat(log): add logLevel feature in the log module

--- src/bots/log.js
+++ src/bots/log.js
@@ -1,15 +1,31 @@
-var config = function (casper) {
+var config = function (casper, pid, level) {
+    // Logging what's happening on the page.
+    casper.on('remote.message', function (message) {
+        log('[log] ' + message, 'INFO');
+    });
+
+    var levelKeys = {
+        none: -1,
+        fatal: 0,
+        error: 1,
+        warn: 2,
+        info: 3,
+        debug: 4,
+        trace: 5
+    };
+
+    level = levelKeys[level ? level.toLowerCase() : 'fatal'] || 0;
+
     return function log() {
         var color = patchRequire(require)('colorizer')
             .create('Colorizer').format;
+
         var d = new Date();
         var args = Array.prototype.slice.call(arguments);
+
         var type = 'DEFAULT';
         var styles = {
             'DEFAULT': {fg: 'white'},
-            'ERROR': {bg: 'red', fg: 'white', bold: true},
-            'INFO': {fg: 'cyan', bold: true},
-            'TRACE': {fg: 'green', bold: true},
             'PARAMETER': {fg: 'cyan'},
             'COMMENT': {fg: 'yellow'},
             'WARNING': {fg: 'red', bold: true},
@@ -21,11 +37,41 @@ var config = function (casper) {
             'SKIP': {fg: 'magenta', bold: true},
             'SKIP_BAR':  {bg: 'magenta', fg: 'white', bold: true},
             'TEST_SUCCESS': {fg: 'green'},
-            'TEST_FAILED': {fg: 'red'}
+            'TEST_FAILED': {fg: 'red'},
+            // TODO remove those before this point.
+            'FATAL': {bg: 'red', fg: 'white', bold: true},
+            'ERROR': {fg: 'red', bold: true},
+            'WARN': {fg: 'yellow', bold: true},
+            'INFO': {fg: 'cyan', bold: true},
+            'DEBUG': {fg: 'white', bold: true},
+            'TRACE': {fg: 'white'}
         };
+
+        var levels = [
+            ['FATAL'],
+            ['ERROR', 'RED_BAR', 'TEST_FAILED'],
+            ['WARN', 'WARN_BAR'],
+            ['INFO_BAR', 'INFO', 'SKIP', 'SKIP_BAR',
+                'SUCCESS', 'TEST_SUCCESS', 'GREEN_BAR'],
+            ['DEFAULT', 'DEBUG'],
+            ['PARAMETER', 'COMMENT', 'TRACE']
+        ];
 
         if (Object.keys(styles).indexOf(args[args.length - 1]) > -1) {
             type = args.pop();
+        }
+
+        // Should we log it ?
+        var toLog = false;
+        for (var i = 0, max = level; i <= max; i += 1) {
+            if (levels[i].indexOf(type) > -1) {
+                toLog = true;
+                break;
+            }
+        }
+
+        if (!toLog) {
+            return;
         }
 
         var dumps = [];
@@ -35,7 +81,9 @@ var config = function (casper) {
             d.getFullYear() + ' ' +
             d.getHours() + ':' +
             d.getMinutes() + ':' +
-            d.getSeconds();
+            d.getSeconds() + ':' +
+            d.getMilliseconds();
+
         out += color(' ' + date + ' ', styles.INFO);
         out += '\n';
 
