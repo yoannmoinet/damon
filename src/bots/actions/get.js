a42147a1f9658ec9c007439307a0734b369ade6e
RuoTai Sun <sunruotai95@gmail.com>
2016-03-03 10:41:55 -0500

    refactor(get): split get module into smaller functions

--- src/bots/actions/get.js
+++ src/bots/actions/get.js
@@ -1,51 +1,55 @@
+function getAttribute (params) {
+    var returnValue = casper.helpers.get.getAttribute(casper, params);
+    if (returnValue !== undefined) {
+        log('got', params.attribute + ' of ' + params.selector,
+            returnValue, 'SUCCESS');
+        return returnValue;
+    }
+    log('no attribute "' + params.attribute +
+        '" found for selector "' + params.selector +
+        '" and ' + (params.modifier || 'without') +
+        ' modifier', 'ERROR');
+    throw new Error('no attribute found');
+}
+
+function getResource (params) {
+    var resourceMatcher = casper.helpers.get.encodeResource(
+        params.resource,
+        params.regexp
+    );
+
+    var returnValue = casper.helpers.get.getResource(
+        casper,
+        resourceMatcher,
+        params.method,
+        params.variable
+    );
+
+    if (returnValue !== undefined) {
+        log('got resource: ' + params.resource, 'SUCCESS');
+        return returnValue;
+    }
+    log('no resource found for: ' + params.resource, 'ERROR');
+    throw new Error('no resource found');
+}
+
+function getVariable (params) {
+    var returnValue = casper.helpers.get.getVariable(casper, params.variable);
+    if (returnValue !== undefined && returnValue !== null) {
+        log('got global variable: ' + params.variable, 'SUCCESS');
+        return returnValue;
+    }
+    log('no variable found for: ' + params.variable, 'ERROR');
+    throw new Error('no value found');
+}
+
 module.exports = function (params) {
-    var get = casper.helpers.get;
-    var returnValue;
     if (params.attribute) {
-
-        returnValue = get.getAttribute(casper, params);
-        if (returnValue !== undefined) {
-            log('got', params.attribute + ' of ' + params.selector,
-                returnValue, 'SUCCESS');
-            return returnValue;
-        }
-        log('no attribute "' + params.attribute +
-            '" found for selector "' + params.selector +
-            '" and ' + (params.modifier || 'without') +
-            ' modifier', 'ERROR');
-        throw new Error('no attribute found');
-
+        return getAttribute(params);
     } else if (params.resource) {
-
-        var resourceMatcher = get.encodeResource(
-            params.resource,
-            params.regexp
-        );
-
-        returnValue = get.getResource(
-            casper,
-            resourceMatcher,
-            params.method,
-            params.variable
-        );
-
-        if (returnValue !== undefined) {
-            log('got resource: ' + params.resource, 'SUCCESS');
-            return returnValue;
-        }
-        log('no resource found for: ' + params.resource, 'ERROR');
-        throw new Error('no resource found');
-
+        return getResource(params);
     } else if (params.variable) {
-
-        returnValue = get.getVariable(casper, params.variable);
-        if (returnValue !== undefined && returnValue !== null) {
-            log('got global variable: ' + params.variable, 'SUCCESS');
-            return returnValue;
-        }
-        log('no variable found for: ' + params.variable, 'ERROR');
-        throw new Error('no value found');
-
+        return getVariable(params);
     }
     log('no action found for ', params, 'ERROR');
     throw new Error('no action found');
