ba88ad420f077b0bdb26b1d3a14fe6e490bdc2c3
Yoann Moinet <yoann.moinet@gmail.com>
2016-03-17 12:44:44 -0400

    feat(phantom): update phantomJS to 2.1.1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ var reporter;
 var env = process.env;
 var phantomjsPath = path.join(__dirname, '../bin');
 var isWin = /^win/.test(process.platform);
-env.PHANTOMJS_EXECUTABLE = phantomjsPath + '/phantomjs_2.0' +
+env.PHANTOMJS_EXECUTABLE = phantomjsPath + '/phantomjs_2.1' +
     (isWin ? '.exe' : '');
 env.PATH += (isWin ? ';' : ':') + phantomjsPath;
 
